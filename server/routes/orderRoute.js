17f3b762564eb4d8b9350f6458a5427e0e6757ed
Afsha-Firdouse <afshaafirdouse@gmail.com>
2025-11-10 12:16:17 +0530

    Update orderRoute.js

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,19 +1,23 @@
 import express from 'express';
-import { placeOrder, getUserOrders, getAllOrders, createRazorpayOrder, verifyPayment } from '../controllers/orderController.js';
+import {
+  placeOrder,
+  getUserOrders,
+  getAllOrders,
+  createRazorpayOrder,
+  verifyPayment,
+} from '../controllers/orderController.js';
 import authUser from '../middlewares/authUser.js';
 import authSeller from '../middlewares/authSeller.js';
 
-
-
-
-
 const orderRouter = express.Router();
 
-orderRouter.post('/cod',authUser, placeOrder)
-orderRouter.post('/razorpay', authUser, createRazorpayOrder)
-orderRouter.post('/verify', authUser, verifyPayment)
-orderRouter.get('/user',authUser, getUserOrders)
-orderRouter.get('/seller',authSeller, getAllOrders)
+// User routes
+orderRouter.post('/cod', authUser, placeOrder);
+orderRouter.post('/razorpay', authUser, createRazorpayOrder);
+orderRouter.post('/verify', authUser, verifyPayment);
+orderRouter.get('/user', authUser, getUserOrders);
 
+// Seller/admin route
+orderRouter.get('/seller', authSeller, getAllOrders);
 
 export default orderRouter;
